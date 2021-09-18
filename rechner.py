7ae29c9da359cc4e0da8f728953a16b0b59aca14
KlevCode <47086564+KlevCode@users.noreply.github.com>
2021-09-18 23:02:02 +0200

    Add files via upload

--- rechner.py
+++ rechner.py
@@ -14,8 +14,8 @@ def button_click(number):
 def button_clear():
   e.delete(0, END)
 
-def button_add(first_number):
-  first_numberg = e.get()
+def button_add():
+  first_number = e.get()
   global f_num
   f_num = int(first_number)
   e.delete(0, END)
@@ -24,6 +24,15 @@ def button_equal():
   second_number = e.get()
   e.delete(0, END)
   e.insert(0, f_num + int(second_number))
+
+def button_subtract():
+  return
+
+def button_multiply():
+  return
+
+def button_divide():
+  return
 
 button_1 = Button(root, text=1, padx=40, pady=20, command=lambda: button_click(1))
 button_2 = Button(root, text=2, padx=40, pady=20, command=lambda: button_click(2))
@@ -36,8 +45,12 @@ button_8 = Button(root, text=8, padx=40, pady=20, command=lambda: button_click(8
 button_9 = Button(root, text=9, padx=40, pady=20, command=lambda: button_click(9))
 button_0 = Button(root, text=0, padx=40, pady=20, command=lambda: button_click(10))
 button_add = Button(root, text="+", padx=39, pady=20, command=button_add)
-button_equal = Button(root, text="=", padx=91, pady=20, command=button_equal)
-button_clear = Button(root, text="Löschen", padx=79, pady=20, command=button_clear)
+button_equal = Button(root, text="=", padx=79, pady=20, command=button_equal)
+button_clear = Button(root, text="Löschen", padx=60, pady=20, command=button_clear)
+
+button_subtract = Button(root, text="-", padx=41, pady=20, command=button_subtract)
+button_multiply = Button(root, text="*", padx=40, pady=20, command=button_multiply)
+button_divide = Button(root, text="/", padx=40, pady=20, command=button_divide)
 
 button_1.grid(row=3, column=0)
 button_2.grid(row=3, column=1)
@@ -56,5 +69,8 @@ button_clear.grid(row=4, column=1, columnspan=2)
 button_add.grid(row=5, column=0)
 button_equal.grid(row=5, column=1, columnspan=2)
 
+button_subtract.grid(row=6, column=0)
+button_multiply.grid(row=6, column=1)
+button_divide.grid(row=6, column=2)
 
 root.mainloop()
